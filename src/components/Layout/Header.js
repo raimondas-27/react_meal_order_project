48e0156a83a45560bcba4253bcf1cd8d6ce982fa
raimondas-27 <raimondasvilnius@yahoo.com>
2021-08-19 08:36:45 +0300

    react header in progress and meals picture configuration

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from "react";
+
+
+const Header = (props) => {
+   return (
+       <div>
+          <header>
+             <h1> React meals</h1>
+             <button> Cart</button>
+          </header>
+          <div>
+             <img src="" alt=""/>
+          </div>
+       </div>
+   )
+}
+
+export default Header;
