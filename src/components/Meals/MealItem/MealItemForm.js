a8b5adc3266233414e8fc902d55bb4e7aca93f18
raimondas-27 <raimondasvilnius@yahoo.com>
2021-08-19 11:31:46 +0300

    passing props.item.id through components done

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -4,10 +4,12 @@ import Input from "../../UI/Input";
 
 
 const MealItemForm = (props) => {
+
+
    return (
        <form className={classes.form}>
           <Input label="Amount" input={{
-             id : "amount",
+             id : props.id,
              type : "number",
              min : 1,
              max : 5,
