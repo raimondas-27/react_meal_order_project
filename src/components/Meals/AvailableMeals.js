d01fc7e2528dd1ab8552c4fe0506fa7d5a018152
raimondas-27 <raimondasvilnius@yahoo.com>
2021-08-19 11:09:38 +0300

    input done, used in mealInputForm

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./AvailableMeals.module.css"
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
    {
@@ -32,7 +33,13 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = (props) => {
 
-   const mealList = DUMMY_MEALS.map((meal) => <li> {meal.name} </li>)
+   const mealList = DUMMY_MEALS.map((meal) => (
+          <MealItem key={meal.id}
+                    name={meal.name}
+                    description={meal.description}
+                    price={meal.price}
+          />
+   ));
 
    return (
        <section className={classes.meals}>
