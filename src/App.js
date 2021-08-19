af1ef33c579c5e0cb20acd383c57e9c00edf6b88
raimondas-27 <raimondasvilnius@yahoo.com>
2021-08-19 08:46:59 +0300

    header styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1> hello </h1>
+      <Header/>
     </div>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,16 @@
 import React, {useState, useEffect} from "react";
-
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
    return (
        <div>
-          <header>
+          <header className={classes.header}>
              <h1> React meals</h1>
              <button> Cart</button>
           </header>
-          <div>
-             <img src="" alt=""/>
+          <div className={classes["main-image"]}>
+             <img src={mealsImage} alt="table full of food"/>
           </div>
        </div>
    )
